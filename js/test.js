aec34431c534c941af912b8ec25da34058aaed0c
Regular-Stone <ConstantPierre.Dev@gmail.com>
2024-03-06 16:58:58 +0100

    update

--- js/test.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    let x = document.forms["myForm"]["fname"]["myForm"]["fprenom"]["myForm"]["femail"].value;
-    if (x == "") {
-      alert("Ce champ doit être remlpi");
-      return false;
-    }
-  }
